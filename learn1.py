f901a4d3ab24c86becfd0c21e932251863fca2df
qd9500805 <9500805@qq.com>
2021-09-17 21:50:43 -0400

    learn1

--- learn1.py
+++ learn1.py
@@ -1,2 +1,19 @@
 car = ['audi','b','benchi']
 print(car)
+print('hello w')
+value = '7'
+if value == '7':
+    print('The value is 7')
+elif value == '8':
+    print('the value is 8')
+else:
+    print('the value is not in')
+value = input ('would you like to continue?')
+if  value =='y' or value =='yes':
+    print('continuing....')
+    print('okok!')
+elif value=='n' or value =='no':
+    print('exiting...')
+else:
+    print('please try again')
+ 
